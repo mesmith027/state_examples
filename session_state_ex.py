0bdc11c3da9c68a9f79e59b04d896d68a40ad272
Marisa Smith <marisa@streamlit.io>
2022-01-07 14:08:45 -0500

    adding callbacks examples

--- session_state_ex.py
+++ session_state_ex.py
@@ -1,11 +1,10 @@
 import streamlit as st
 
 st.title("Session state examples")
-options = ["Button control", "Counter example", "Toggle example"]
+options = ["Button control", "Counter example", "Toggle example", "Link widgets", "Callbacks"]
 
 with st.sidebar:
     page = st.radio("Pick an example", options)
-
 
 if page == options[0]:
     st.subheader(options[0])
@@ -19,7 +18,7 @@ if page == options[0]:
             st.session_state["open"] = True
             return
 
-        # create teh button that the person presses to run the app
+        # create the button that the person presses to run the app
         st.button("Press me to run the rest of the app", on_click=app_open)
 
         # the rest of the app runs based on the session state value
@@ -66,3 +65,58 @@ elif page == options[2]:
             st.write("You opened the app! 🥳🥳🥳")
         else:
             st.write("Open the app by pressing the button above 👆")
+elif page == options[3]:
+    st.subheader(options[3])
+    with st.echo():
+        st.write("work in progress")
+elif page == options[4]:
+    st.subheader(options[4])
+    st.write("""Use a callback function when you need a variable to be updated before your script re-runs.
+    This is used when you want a widget to reflect the changes before it is rendered again on the page.""")
+    st.write("Using a callback function with a button")
+    with st.echo():
+        # create callback function, this executes BEFORE the script reruns
+        def button_pressed():
+            if st.session_state.btn_name == "Next":
+                # will switch the button to say "Back" and turn page to true
+                st.session_state.btn_name = "Back"
+                st.session_state.page = True
+            else:
+                # button will say "next", and set page to False
+                st.session_state.btn_name = "Next"
+                st.session_state.page = False
+            return
+
+        # put the string name of a button in state
+        if "btn_name" not in st.session_state:
+            st.session_state.btn_name = "Next"
+            st.session_state.page = False
+
+        # make a button that's name will change, and will change page value in state
+        st.button(st.session_state.btn_name, on_click = button_pressed)
+
+        # depending on what "page" you are on, change what display's to the screen
+        if st.session_state.page:
+            st.write("You moved to next")
+            st.balloons()
+        else:
+            st.write("Click next to move on")
+    st.write("---")
+    st.write("Using a callback function with sliders")
+    with st.echo():
+        #define the call back functions
+        def slider_one():
+            # depending on the value of slider 1, we will change slider 2's value
+            st.session_state.sldr_2 = float(st.session_state.sldr_1/2)
+            return
+
+        def slider_two():
+            # update slider 1's value based on the value for slider 2
+            st.session_state.sldr_1 = int(st.session_state.sldr_2*2)
+            return
+
+        # slider 1's value will be 2 times slider 2's value, and always be between 1-10
+        st.slider("Slider 1", min_value = 1,max_value = 10, on_change=slider_one, key="sldr_1")
+
+        # slider 2's value will be half of slider 1's value
+        st.slider("Slider 2",min_value=0.5,max_value=5.0,step=0.5, on_change=slider_two, key ="sldr_2")
